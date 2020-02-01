3ce554f479b5b2774ab47a7face7d179c6e788ce
Robert Fine <rob.fine09@gmail.com>
2020-01-31 23:13:33 -0600

    timer and buttons and border added

--- js/game.js
+++ js/game.js
@@ -1,6 +1,10 @@
-let output = document.querySelector('#output');
+const container = document.querySelector("#container");
+const playArea = document.querySelector("#playArea");
+const startBtn = document.querySelector("#start");
+const finishBtn = document.querySelector("#finish");
+const timerDisplay = document.querySelector("#timer");
 
-output.innerHTML = `
+playArea.innerHTML = `
 <div class='tile'>
 <div class='wall square'>
 
@@ -8,11 +12,60 @@ output.innerHTML = `
 </div>
 `;
 
-let walls = output
-  .querySelectorAll('.wall')
+let walls;
 
-  .addEventListener('mouseover', touchWall);
-
-function touchWall(e) {
-  console.log('You touched a wall');
+function findWalls() {
+  walls = container.querySelectorAll(".wall");
 }
+
+startBtn.addEventListener("click", startGame);
+
+function startGame(e) {
+  e.preventDefault();
+  findWalls();
+  timer();
+  walls.forEach(wall => {
+    wall.addEventListener("mouseover", touchWall);
+  });
+
+  function touchWall(e) {
+    console.log("You touched a wall");
+  }
+}
+
+// timer function
+let min = 00;
+let sec = 00;
+let milSec = 00;
+
+function add() {
+  milSec += 5;
+  if (milSec >= 100) {
+    milSec = 0;
+    sec++;
+    if (sec >= 60) {
+      sec = 0;
+      min++;
+    }
+  }
+  timer();
+}
+function timer() {
+  t = setTimeout(add, 50);
+  timerDisplay.innerHTML =
+    (min ? (min > 9 ? min : "0" + min) : "00") +
+    ":" +
+    (sec ? (sec > 9 ? sec : "0" + sec) : "00") +
+    ":" +
+    (milSec > 9 ? milSec : "0" + milSec);
+}
+
+function stopTimer() {
+  clearTimeout(t);
+}
+
+finishBtn.addEventListener("click", stopTimer);
+
+// `
+// ${min}:${sec}:${milSec}
+// `;
