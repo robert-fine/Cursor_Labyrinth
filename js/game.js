c9afc87ab56f6aaba7d9f3d0b348b9b54e84d703
Robert Fine <rob.fine09@gmail.com>
2020-02-03 16:46:55 -0600

    rotate still does not work

--- js/game.js
+++ js/game.js
@@ -1,8 +1,8 @@
-const container = document.querySelector("#container");
-const playArea = document.querySelector("#playArea");
-const startBtn = document.querySelector("#start");
-const finishBtn = document.querySelector("#finish");
-const timerDisplay = document.querySelector("#timer");
+const container = document.querySelector('#container');
+const playArea = document.querySelector('#playArea');
+const startBtn = document.querySelector('#start');
+const finishBtn = document.querySelector('#finish');
+const timerDisplay = document.querySelector('#timer');
 
 // playArea.innerHTML = `
 // `;
@@ -11,25 +11,26 @@ const timerDisplay = document.querySelector("#timer");
 let walls;
 
 function findWalls() {
-  walls = container.querySelectorAll(".wall");
+  walls = container.querySelectorAll('.wall');
 }
 
 function activateWalls() {
   walls.forEach(wall => {
-    wall.addEventListener("mouseover", touchWall);
+    wall.addEventListener('mouseover', touchWall);
   });
   function touchWall() {
     stopTimer();
+    stopRotate();
     // Deactivate walls
     walls.forEach(wall => {
-      wall.removeEventListener("mouseover", touchWall);
+      wall.removeEventListener('mouseover', touchWall);
     });
   }
 }
 
 // Game
-startBtn.addEventListener("click", startGame);
-finishBtn.addEventListener("click", stopTimer);
+startBtn.addEventListener('click', startGame);
+finishBtn.addEventListener('click', stopTimer);
 
 function startGame(e) {
   e.preventDefault();
@@ -38,25 +39,63 @@ function startGame(e) {
   timer();
   activateWalls();
   findTiles();
+  resetTileRotation();
+  rotateTimer();
 }
 
 // Rotate
 let tiles;
 
 function findTiles() {
-  tiles = playArea.querySelectorAll(".tile");
-}
-
-function rotateLeft() {
-  tiles.forEach(tile => {
-    tile.classList.add("rotateActivate");
-    // tile.classList.remove('rotateActivate')
-  });
-  console.log("rotate");
+  tiles = playArea.querySelectorAll('.tile');
 }
 
 function rotate() {
-  setTimeout(rotateLeft, 5000);
+  tiles.forEach(tile => {
+    // if (tile.classList != 'rotate1') {
+    //   console.log('adding rotate1');
+    //   tile.classList.add('rotate1');
+    // } else if (tile.classList !== 'rotate2') {
+    //   console.log('adding rotate2');
+    //   tile.classList.add('rotate2');
+    // }
+
+    // MAYBE USE A SWITCH STATEMENT?
+    switch (tile.classList.remove) {
+      case 'rotate3':
+        resetTileRotation();
+        tile.classList.add('rotate0');
+        rotateTimer();
+        break;
+      case 'rotate2':
+        tile.classList.add('rotate3');
+        rotateTimer();
+        break;
+      case 'rotate1':
+        tile.classList.add('rotate2');
+        rotateTimer();
+        break;
+      case 'rotate0':
+        tile.classList.add('rotate1');
+        rotateTimer();
+    }
+  });
+  console.log('rotate');
+  rotateTimer();
+}
+
+function rotateTimer() {
+  rt = setTimeout(rotate, 5000);
+}
+
+function stopRotate() {
+  clearTimeout(rt);
+}
+
+function resetTileRotation() {
+  tiles.forEach(tile => {
+    tile.classList.remove('rotate1', 'rotate2', 'rotate3');
+  });
 }
 
 // timer function
@@ -79,11 +118,11 @@ function add() {
 function timer() {
   t = setTimeout(add, 50);
   timerDisplay.innerHTML =
-    (min ? (min > 9 ? min : "0" + min) : "00") +
-    ":" +
-    (sec ? (sec > 9 ? sec : "0" + sec) : "00") +
-    ":" +
-    (milSec > 9 ? milSec : "0" + milSec);
+    (min ? (min > 9 ? min : '0' + min) : '00') +
+    ':' +
+    (sec ? (sec > 9 ? sec : '0' + sec) : '00') +
+    ':' +
+    (milSec > 9 ? milSec : '0' + milSec);
 }
 
 function stopTimer() {
@@ -95,3 +134,5 @@ function resetTimer() {
   sec = 0;
   milSec = 0;
 }
+
+// add rotate1 (90deg), rotate2 (180deg), rotate3 (270deg) then erase for rotate4 and start over (use if statements)
