b3d39322304ad4c584786574a1174878bb8dbb22
Robert Fine <rob.fine09@gmail.com>
2020-02-04 15:16:38 -0600

    rotate and color change added. 1st level added.

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
-const container = document.querySelector("#container");
-const playArea = document.querySelector("#playArea");
-const startBtn = document.querySelector("#start");
-const finishBtn = document.querySelector("#finish");
-const timerDisplay = document.querySelector("#timer");
-let tiles = document.querySelectorAll(".tile");
-let paths = document.querySelectorAll(".path");
+const container = document.querySelector('#container');
+const playArea = document.querySelector('#playArea');
+const startBtn = document.querySelector('#start');
+const finishBtn = document.querySelector('#finish');
+const timerDisplay = document.querySelector('#timer');
+let tiles = document.querySelectorAll('.tile');
+let paths = document.querySelectorAll('.path');
 
 // playArea.innerHTML = `
 // `;
@@ -13,26 +13,34 @@ let paths = document.querySelectorAll(".path");
 let walls;
 
 function findWalls() {
-  walls = container.querySelectorAll(".wall");
+  walls = container.querySelectorAll('.wall');
 }
 
 function activateWalls() {
   walls.forEach(wall => {
-    wall.addEventListener("mouseover", touchWall);
+    wall.setAttribute('style', 'background: var(--wall-color)');
+    wall.addEventListener('mouseover', touchWall);
   });
   function touchWall() {
     stopTimer();
     stopRotate();
-    // Deactivate walls
+    // Deactivate walls & change color
     walls.forEach(wall => {
-      wall.removeEventListener("mouseover", touchWall);
+      wall.setAttribute('style', 'background: var(--fail)');
     });
+    deactivateWalls();
   }
 }
 
+function deactivateWalls() {
+  walls.forEach(wall => {
+    wall.removeEventListener('mouseover', touchWall);
+  });
+}
+
 // Game
-startBtn.addEventListener("click", startGame);
-finishBtn.addEventListener("click", stopTimer);
+startBtn.addEventListener('click', startGame);
+finishBtn.addEventListener('click', stopTimer);
 
 function startGame(e) {
   e.preventDefault();
@@ -45,46 +53,35 @@ function startGame(e) {
   rotateTimer();
 }
 
+function finishGame(e) {
+  e.preventDefault();
+  stopTimer();
+  stopRotate();
+  deactivateWalls();
+}
+
 tiles.forEach(tile => {
   tile.onmouseenter = function(e) {
-    e.target.classList.add("mouseIsOver");
+    e.target.classList.add('mouseIsOver');
   };
   tile.onmouseleave = function(e) {
-    e.target.classList.remove("mouseIsOver");
+    e.target.classList.remove('mouseIsOver');
   };
-
-  // tile.addEventListener("mouseover", console.log("mouseisover"));
-  // tile.addEventListener("mouseout", removeMouseIsOver);
-
-  // function mouseIsOver() {
-  //   tile.classList.add("mouseIsOver");
-  // }
-
-  // function removeMouseIsOver() {
-  //   tile.classList.remove("mouseIsOver");
-  // }
 });
 
-// Paths
-// paths.forEach(path => {
-
-// })
-
-// Tiles
-
 function findTiles() {
-  tiles = playArea.querySelectorAll(".tile");
+  tiles = playArea.querySelectorAll('.tile');
 }
 // Tile Rotation
 let deg = 0;
 function rotate() {
   deg += 90;
   tiles.forEach(tile => {
-    if (!tile.classList.contains("mouseIsOver")) {
-      tile.setAttribute("style", `transform: rotate(${deg}deg)`);
+    if (!tile.classList.contains('mouseIsOver')) {
+      tile.setAttribute('style', `transform: rotate(${deg}deg)`);
     }
   });
-  console.log("rotate");
+  console.log('rotate');
   rotateTimer();
 }
 
@@ -98,7 +95,7 @@ function stopRotate() {
 
 function resetTileRotation() {
   tiles.forEach(tile => {
-    tile.setAttribute("style", "transform: rotate(0deg)");
+    tile.setAttribute('style', 'transform: rotate(0deg)');
   });
   deg = 0;
 }
@@ -123,11 +120,11 @@ function add() {
 function timer() {
   t = setTimeout(add, 50);
   timerDisplay.innerHTML =
-    (min ? (min > 9 ? min : "0" + min) : "00") +
-    ":" +
-    (sec ? (sec > 9 ? sec : "0" + sec) : "00") +
-    ":" +
-    (milSec > 9 ? milSec : "0" + milSec);
+    (min ? (min > 9 ? min : '0' + min) : '00') +
+    ':' +
+    (sec ? (sec > 9 ? sec : '0' + sec) : '00') +
+    ':' +
+    (milSec > 9 ? milSec : '0' + milSec);
 }
 
 function stopTimer() {
